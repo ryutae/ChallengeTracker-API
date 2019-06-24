46ca5008ccbaa383b8f88a0b320643d8cc2e821a
ryuta <ryuta.eguchi@gmail.com>
2019-06-24 16:04:55 -0700

    Add user route

--- User/user.routes.js
+++ User/user.routes.js
@@ -4,6 +4,7 @@ const userRouter = express.Router()
 const jsonBodyParser = express.json()
 const path = require('path')
 const UserService = require('../User/user-service')
+const { requireAuth } = require('../middleware/jwt-auth')
 
 userRouter
   .route('/:user_id')
@@ -19,9 +20,11 @@ userRouter
   })
 
 userRouter
-  .route('/:user_id/:group_id')
+  .route('/group/:group_id')
+  .all(requireAuth)
   .get((req, res, next) => {
-    const { group_id, user_id } = req.params
+    const { group_id } = req.params
+    const user_id = req.user.id
       UserService.getUserGroupRef(req.app.get('db'), group_id, user_id)
       .then(user => {
         res.status(200).json({
